8eab987773ee0cb071ce0040b254e76a4af93d29
Dan Howe <d.howe@wrl.unsw.edu.au>
2018-11-30 10:36:49 +1100

    Add 'setup.py'

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='pygtide',
+    version='0.2',
+    packages=['pygtide', 'etpred'],
+    package_data={'etpred': ['*.pyd', 'commdat/*']},
+    install_requires=['numpy', 'pandas'],
+    author='Gabriel C. Rau',
+    author_email='gabriel@hydrogeo.science',
+    url='http://doi.org/10.5281/zenodo.1346664',
+    description=('A Python module and wrapper for ETERNA PREDICT to compute '
+                 'gravitational tides on Earth'),
+    long_description=long_description,
+    long_description_content_type='text/markdown')
