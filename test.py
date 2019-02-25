6736beb37b3dc8b6126c79be3983e21923cc3f94
trichter <tom.eulenfeld@bgr.de>
2019-02-25 10:58:56 +0100

    update test.py

--- test.py
+++ test.py
@@ -7,10 +7,12 @@ pt = pygtide.pygtide()
 
 # define a start date
 start = dt.datetime(2017,1,1)
+print('yeah')
 
-# calculate the gravitational tides 
-# BUG: CURRENT COMBINATION CRASHING THE CODE (1 / 23) 
+# calculate the gravitational tides
+# BUG: CURRENT COMBINATION CRASHING THE CODE (1 / 23)
 pt.predict(-33.33, 151.33, 589.33, start, 2, 249, fileprd=1)
+#pt.predict(-20.82071, -70.15288, 830.0, start, 3, 600)
 
 # retrieve the results as Pandas data frame
 data = pt.results()
@@ -20,6 +22,8 @@ print(data)
 
 # update the pole coordinates and UT1 time database
 pt.update_etpolut1()
+pt.update_etddt()
+pt.etpolut1_dat2bin()
 
 # update the time database
 #pt.update_etddt()
