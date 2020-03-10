5b3d53a5a3670ea1f12077854c9d83720cedc280
trichter <tom.eulenfeld@uni-jena.de>
2020-03-10 17:13:38 +0100

    small changes to test

--- test.py
+++ test.py
@@ -1,18 +1,14 @@
 #%% PyGTide class test
 import pygtide
-import datetime as dt
 
 # create a PyGTide object
 pt = pygtide.pygtide()
 
-# define a start date
-start = dt.datetime(2017,1,1)
-print('yeah')
-
 # calculate the gravitational tides
 # BUG: CURRENT COMBINATION CRASHING THE CODE (1 / 23)
-pt.predict(-33.33, 151.33, 589.33, start, 2, 249, fileprd=1)
-#pt.predict(-20.82071, -70.15288, 830.0, start, 3, 600)
+#pt.predict(-33.33, 151.33, 589.33, start, 2, 249, fileprd=0)
+pt.predict(-20.82071, -70.15288, 830.0, '2017-01-01', 6, 600, statazimut=90,
+           tidalcompo=8)
 
 # retrieve the results as Pandas data frame
 data = pt.results()
@@ -21,9 +17,11 @@ data = pt.results()
 print(data)
 
 # update the pole coordinates and UT1 time database
-pt.update_etpolut1()
-pt.update_etddt()
-pt.etpolut1_dat2bin()
+#pt.update_etpolut1()
+
+#pt.update_etddt()
+
+#pt.etpolut1_dat2bin()
 
 # update the time database
 #pt.update_etddt()
