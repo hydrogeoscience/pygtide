b68da5043e8931fd405a1ebd052e03c3f39a3734
Roelof Rietbroek <r.rietbroek@utwente.nl>
2022-04-04 15:10:20 +0200

    removed dependency on astropy and fixed broken link for final2000A.all data

--- pygtide/__init__.py
+++ pygtide/__init__.py
@@ -1,4 +1,4 @@
 from pygtide.core import pygtide
 from pygtide.core import predict_series, predict_spectrum, predict_table
 from pygtide.core import plot_series, plot_spectrum
-from pygtide.tests import test
+from pygtide.tests import test,test_refresh
