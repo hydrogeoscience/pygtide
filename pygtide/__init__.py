c2c149035f47b7965f76ed7e3c51a2da958302f3
trichter <tom.eulenfeld@uni-jena.de>
2022-04-26 15:38:18 +0200

    add changelog, version 0.4.0

--- pygtide/__init__.py
+++ pygtide/__init__.py
@@ -3,4 +3,4 @@ from pygtide.core import predict_series, predict_spectrum, predict_table
 from pygtide.core import plot_series, plot_spectrum, update_data_files
 from pygtide.tests import test
 
-__version__ = '0.3+trichters.fork'
+__version__ = '0.4.0+trichters.fork'
