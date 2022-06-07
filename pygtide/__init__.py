6e133297e60577e2f010b0cc8e38a393f3cf0d2b
trichter <tom.eulenfeld@uni-jena.de>
2022-06-07 14:51:58 +0200

    rename update.py to not clash with update function, update ci tests

--- pygtide/__init__.py
+++ pygtide/__init__.py
@@ -2,5 +2,5 @@ from pygtide.core import pygtide
 from pygtide.core import predict_series, predict_spectrum, predict_table
 from pygtide.core import plot_series, plot_spectrum
 from pygtide.tests import test
-from pygtide.update import update_etpred_db, update
+from pygtide.update_etpred_data import update
 __version__ = '0.6.0'
