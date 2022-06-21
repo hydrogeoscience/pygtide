1bd258c44528d4bb3cceaedaf5d6c06e15fb143f
Gabriel Rau <32972360+hydrogeoscience@users.noreply.github.com>
2022-06-21 17:06:57 +1000

    v0.7.1

--- pygtide/__init__.py
+++ pygtide/__init__.py
@@ -3,4 +3,4 @@ from pygtide.core import predict_series, predict_spectrum, predict_table
 from pygtide.core import plot_series, plot_spectrum
 from pygtide.tests import test
 from pygtide.update_etpred_data import update
-__version__ = '0.7.0'
+__version__ = '0.7.1'
