8abce049ec86eec1918632e294d2e8bd90c9ee76
trichter <tom.eulenfeld@uni-jena.de>
2022-07-01 10:17:15 +0200

    try to fix

--- setup.py
+++ setup.py
@@ -18,7 +18,6 @@ VERSION = find_version('pygtide', '__init__.py')
 extra_link_args = None
 if platform.system() == 'Windows' and os.environ['CIBW_BUILD']:
     # provide extra link args for wheel building on Windows
-    raise
     extra_link_args = ('-static', '-static-libgfortran', '-static-libgcc')
 ext = [Extension(name='pygtide.etpred', sources=['src/etpred.f90'],
                  extra_link_args=extra_link_args)]
