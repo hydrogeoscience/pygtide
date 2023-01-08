c8cda534a4c8c743db08f5ab3864b7fb1814e908
Gabriel C Rau <gabriel@hydrogeo.science>
2023-01-09 10:00:56 +1100

    Depth update 2

--- pygtide/core.py
+++ pygtide/core.py
@@ -322,7 +322,7 @@ class pygtide(object):
             raise ValueError('Longitude exceeds permissible range!')
         else:
             argsin[1] = longitude
-        if not (-10000 <= height <= 10000):
+        if not (-1000000 <= height <= 10000):
             raise ValueError('Height exceeds permissible range!')
         else:
             argsin[2] = height
